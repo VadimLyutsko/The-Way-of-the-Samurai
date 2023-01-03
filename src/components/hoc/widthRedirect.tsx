d6077cd44ff8cd42d12e541da83a6d26eb03d585
vadim <vadim_lyutsko@mail.ru>
2023-01-03 23:07:02 +0300

    Add type of my HOC - widthAuthRedirect

--- src/components/hoc/widthRedirect.tsx
+++ src/components/hoc/widthRedirect.tsx
@@ -3,22 +3,46 @@ import {Redirect} from 'react-router-dom';
 import {StateType} from '../../redux/redux-store';
 import {connect} from 'react-redux';
 
-let mapStateToPropsForRedirect = (state: StateType) => ({
+
+type MapStatePropsType = {
+    isAuth:boolean
+}
+
+let mapStateToPropsForRedirect = (state: StateType):MapStatePropsType => ({
     isAuth: state.auth.isAuth
 });
 
-export const widthAuthRedirect = (Component: any) => {
+export function  widthAuthRedirect  <T>(Component: React.ComponentType<T>)  {
 
-    class RedirectComponent extends React.Component<any, any> {
-        render() {
-            if (!this.props.isAuth) return <Redirect to={'/login'}/>;
+    const RedirectComponent = (props: MapStatePropsType) => {
 
-            return <Component {...this.props} />;
-        }
-    }
+        let {isAuth, ...restProps} = props
+
+        if (isAuth) return <Redirect to={'/login'}/>;
+
+        return <Component {...restProps as T} />;
+    };
 
     // let ConnectedAuthRedirectComponent = connect(mapStateToPropsForRedirect)(RedirectComponent)
     // return ConnectedAuthRedirectComponent
 
     return connect(mapStateToPropsForRedirect)(RedirectComponent);
 };
+
+// let mapStateToPropsForRedirect = (state: StateType)=>({
+//     isAuth: state.auth.isAuth
+// })
+//
+// export const widthAuthRedirect = (Component:any) => {
+//
+//     class RedirectComponent extends  React.Component<any, any> {
+//         render() {
+//             if(!this.props.isAuth) return <Redirect to={"/login"}/>
+//
+//             return <Component {...this.props} />
+//         }
+//     }
+//
+//     return connect(mapStateToPropsForRedirect)(RedirectComponent)
+//
+// };
